5c7e21ef066523added589b05eeea0a6b3793d9d
Kirk.Wang <kirk.w.wang@gmail.com>
2019-02-23 20:53:40 +0800

    logger

--- main.go
+++ main.go
@@ -2,7 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/lotteryjs/winning/log"
+	"math/rand"
+	"os"
+	"time"
 )
+
+// Logger
+var logger *log.Logger
+
+// The only one init function.
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	log.SetLevel("warn")
+	logger = log.NewLogger(os.Stdout)
+}
 
 // Entry point.
 func main() {
