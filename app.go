7efbc1a85b8d0dbea48a153219a7d1fcfa2e7cf0
Kirk.Wang <kirk.w.wang@gmail.com>
2019-02-27 20:40:55 +0800

    update project structure

---
+++ app.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var (
+	// Version the version of Gotify.
+	Version = "unknown"
+	// Commit the git commit hash of this version.
+	Commit = "unknown"
+	// BuildDate the date on which this binary was build.
+	BuildDate = "unknown"
+	// Mode the build mode
+	Mode = mode.Dev
+)
+
+func main() {
+	vInfo := &model.VersionInfo{Version: Version, Commit: Commit, BuildDate: BuildDate}
+	mode.Set(Mode)
+
+	fmt.Println("Starting Gotify version", vInfo.Version+"@"+BuildDate)
+	rand.Seed(time.Now().UnixNano())
+	conf := config.Get()
+
+	if conf.PluginsDir != "" {
+		if err := os.MkdirAll(conf.PluginsDir, 0755); err != nil {
+			panic(err)
+		}
+	}
+	if err := os.MkdirAll(conf.UploadedImagesDir, 0755); err != nil {
+		panic(err)
+	}
+
+	db, err := database.New(conf.Database.Dialect, conf.Database.Connection, conf.DefaultUser.Name, conf.DefaultUser.Pass, conf.PassStrength, true)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	engine, closeable := router.Create(db, vInfo, conf)
+	defer closeable()
+
+	runner.Run(engine, conf)
+}
