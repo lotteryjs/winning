f533e203bb9599d0cdc9fa1f233ec23e6790e847
Kirk.Wang <kirk.w.wang@gmail.com>
2019-02-27 22:02:49 +0800

    add config.yml

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/gotify/configor"
+)
+
+// Configuration is stuff that can be configured externally per env variables or config file (config.yml).
+type Configuration struct {
+	Server struct {
+		Port            int `default:"80"`
+		ResponseHeaders map[string]string
+	}
+	Database struct {
+		Dialect    string `default:"sqlite3"`
+		Connection string `default:"data/gotify.db"`
+	}
+	DefaultUser struct {
+		Name string `default:"admin"`
+		Pass string `default:"admin"`
+	}
+	PassStrength int `default:"10"`
+}
+
+// Get returns the configuration extracted from env variables or config file.
+func Get() *Configuration {
+	conf := new(Configuration)
+	err := configor.New(&configor.Config{EnvironmentPrefix: "WINNING"}).Load(conf, "config.yml", "/etc/gotify/config.yml")
+	if err != nil {
+		panic(err)
+	}
+	return conf
+}
