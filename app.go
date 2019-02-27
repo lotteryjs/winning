567bed4e79f56fab0dffbf4eafe56c86d5daa6a2
Kirk.Wang <kirk.w.wang@gmail.com>
2019-02-27 21:36:20 +0800

    go1.12

--- app.go
+++ app.go
@@ -5,6 +5,8 @@ import (
 	"math/rand"
 	"os"
 	"time"
+
+	"github.com/lotteryjs/winning/mode"
 )
 
 var (
@@ -19,30 +21,5 @@ var (
 )
 
 func main() {
-	vInfo := &model.VersionInfo{Version: Version, Commit: Commit, BuildDate: BuildDate}
-	mode.Set(Mode)
 
-	fmt.Println("Starting Gotify version", vInfo.Version+"@"+BuildDate)
-	rand.Seed(time.Now().UnixNano())
-	conf := config.Get()
-
-	if conf.PluginsDir != "" {
-		if err := os.MkdirAll(conf.PluginsDir, 0755); err != nil {
-			panic(err)
-		}
-	}
-	if err := os.MkdirAll(conf.UploadedImagesDir, 0755); err != nil {
-		panic(err)
-	}
-
-	db, err := database.New(conf.Database.Dialect, conf.Database.Connection, conf.DefaultUser.Name, conf.DefaultUser.Pass, conf.PassStrength, true)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	engine, closeable := router.Create(db, vInfo, conf)
-	defer closeable()
-
-	runner.Run(engine, conf)
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module github.com/lotteryjs/winning
 
+go 1.12
+
 require (
 	github.com/gin-contrib/sse v0.0.0-20190226023149-996076df5b33 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v1.3.0 // indirect
 	github.com/mattn/go-isatty v0.0.6 // indirect
 	github.com/stretchr/testify v1.3.0
 	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
