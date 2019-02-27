50c6959f147d0c9f750ed47237c95f56640e88a9
Kirk.Wang <kirk.w.wang@gmail.com>
2019-02-28 00:19:19 +0800

    added database.go

---
+++ database/database.go
@@ -0,0 +1,66 @@
+package database
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"    // enable the mysql dialect
+	_ "github.com/jinzhu/gorm/dialects/postgres" // enable the postgres dialect
+	_ "github.com/jinzhu/gorm/dialects/sqlite"   // enable the sqlite3 dialect
+	"github.com/lotteryjs/winning/auth/password"
+	"github.com/lotteryjs/winning/model"
+)
+
+var mkdirAll = os.MkdirAll
+
+// New creates a new wrapper for the gorm database framework.
+func New(dialect, connection, defaultUser, defaultPass string, strength int, createDefaultUserIfNotExist bool) (*GormDatabase, error) {
+	createDirectoryIfSqlite(dialect, connection)
+
+	db, err := gorm.Open(dialect, connection)
+	if err != nil {
+		return nil, err
+	}
+
+	// We normally don't need that much connections, so we limit them. F.ex. mysql complains about
+	// "too many connections", while load testing Gotify.
+	db.DB().SetMaxOpenConns(10)
+
+	if dialect == "sqlite3" {
+		// We use the database connection inside the handlers from the http
+		// framework, therefore concurrent access occurs. Sqlite cannot handle
+		// concurrent writes, so we limit sqlite to one connection.
+		// see https://github.com/mattn/go-sqlite3/issues/274
+		db.DB().SetMaxOpenConns(1)
+	}
+
+	db.AutoMigrate(new(model.User), new(model.Application), new(model.Message), new(model.Client), new(model.PluginConf))
+	userCount := 0
+	db.Find(new(model.User)).Count(&userCount)
+	if createDefaultUserIfNotExist && userCount == 0 {
+		db.Create(&model.User{Name: defaultUser, Pass: password.CreatePassword(defaultPass, strength), Admin: true})
+	}
+
+	return &GormDatabase{DB: db}, nil
+}
+
+func createDirectoryIfSqlite(dialect string, connection string) {
+	if dialect == "sqlite3" {
+		if _, err := os.Stat(filepath.Dir(connection)); os.IsNotExist(err) {
+			if err := mkdirAll(filepath.Dir(connection), 0777); err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+// GormDatabase is a wrapper for the gorm framework.
+type GormDatabase struct {
+	DB *gorm.DB
+}
+
+// Close closes the gorm database connection.
+func (d *GormDatabase) Close() {
+	d.DB.Close()
+}
--- go.mod
+++ go.mod
@@ -5,9 +5,14 @@ go 1.12
 require (
 	github.com/gin-contrib/sse v0.0.0-20190226023149-996076df5b33 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
 	github.com/golang/protobuf v1.3.0 // indirect
+	github.com/jinzhu/gorm v1.9.2
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/lib/pq v1.0.0 // indirect
 	github.com/lotteryjs/configor v1.0.2
 	github.com/mattn/go-isatty v0.0.6 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
 	github.com/stretchr/testify v1.3.0
 	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
